9f88f4c07f964b1a26c2f21ff4e497e998db9e2c
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-14 16:27:00 +0400

    calculator changes commit

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -2,7 +2,6 @@ function calculate(){
   // 1. init & validate
   const getSide = id => input.get(id).optional().positive().val();
   const getAngle = id => input.get(id).optional().positive().lt(180).val();
-  const check = values => values.every(item=>item); 
   let a = getSide('side_a');
   let b = getSide('side_b');
   let c = getSide('side_c');
@@ -10,13 +9,13 @@ function calculate(){
   let B = getAngle('angle_b');
   let C = getAngle('angle_c');
   input.silent = false;
-  if(check([A,B,C]) || [a,b,c,A,B,C].reduce((count,item)=>item?count+1:count,0) != 3){
-    input.error([],"Please provide 3 positive values, including at least one side");
+  if(A&&B&&C || [a,b,c,A,B,C].reduce((count,item)=>item?count+1:count,0) != 3){
+    input.error([],"Please provide only 3 positive values, including at least one side");
   }
-  if(check([A,B])&&(A+B>=180) || check([B,C])&&(B+C>=180) || check([A,C])&&(A+C>=180)){
+  if(A&&B&&(A+B>=180) || B&&C&&(B+C>=180) || A&&C&&(A+C>=180)){
     input.error(['angle_a','angle_b','angle_c'],"The sum of the two angles must be less than 180° or π radians");
   }
-  if(check([a,b,c]) && ((a+b<=c) || (a+c<=b) || (b+c<=a))){
+  if(a&&b&&c && ((a+b<=c) || (a+c<=b) || (b+c<=a))){
     input.error(['side_a','side_b','side_c'],"The sum of two sides must be larger than the third");
   }
   if(!input.valid()) return;
@@ -45,43 +44,43 @@ function calculate(){
 
   try{
     // Case 1: 3 sides 
-    if(check([a,b,c])){
+    if(a&&b&&c){
       A = calcAngle3s(a,b,c);
       B = calcAngle3s(b,a,c);
       C = calcAngle3s(c,a,b);
     }
 
     // Case 2: 2 sides, 1 angle between
-    else if(check([b,C,a])){
+    else if(b&&C&&a){
       c = calcSideA2s(C,a,b);
       B = calcAngle3s(b,a,c);
       A = calcAngle3s(a,b,c);
     }
-    else if(check([a,B,c])){
+    else if(a&&B&&c){
       b = calcSideA2s(B,a,c);
       A = calcAngle3s(a,b,c);
       C = calcAngle3s(c,a,b);
     }
-    else if(check([c,A,b])){
+    else if(c&&A&&b){
       a = calcSideA2s(A,b,c);
       C = calcAngle3s(c,a,b);
       B = calcAngle3s(b,a,c);
     }
 
     // Case 3: 2 sides, 1 angle
-    else if(check([c,b,C]) || check([b,c,B])){
+    else if(c&&b&&C || b&&c&&B){
       if(C) B = calcAngle2sA(b,c,C);
       if(B) C = calcAngle2sA(c,b,B);
       A = calcAngle2A(B,C);
       a = calcSideA2s(A,b,c);
     }
-    else if(check([b,a,B]) || check([a,b,A])){
+    else if(b&&a&&B || a&&b&&A){
       if(B) A = calcAngle2sA(a,b,B);
       if(A) B = calcAngle2sA(b,a,A);
       C = calcAngle2A(A,B);
       c = calcSideA2s(C,a,b);
     }
-    else if(check([a,c,A]) || check([c,a,C])){
+    else if(a&&c&&A || c&&a&&C){
       if(A) C = calcAngle2sA(c,a,A);
       if(C) A = calcAngle2sA(a,c,C);
       B = calcAngle2A(A,C);
@@ -89,36 +88,36 @@ function calculate(){
     }
 
     // Case 4: 2 angles, 1 side between
-    else if(check([A,b,C])){
+    else if(A&&b&&C){
       B = calcAngle2A(A,C);
       a = calcSideAsA(A,b,B);
       c = calcSideA2s(C,a,b);
     }
-    else if(check([C,a,B])){
+    else if(C&&a&&B){
       A = calcAngle2A(B,C);
       c = calcSideAsA(C,a,A);
       b = calcSideA2s(B,a,c);
     }
-    else if(check([B,c,A])){
+    else if(B&&c&&A){
       C = calcAngle2A(A,B);
       b = calcSideAsA(B,c,C);
       a = calcSideA2s(A,b,c);
     }
 
     // Case 5: 2 angles, 1 side
-    else if(check([A,C,a])||check([C,A,c])){
+    else if(A&&C&&a||C&&A&&c){
       if(a) c = calcSideAsA(C,a,A);
       if(c) a = calcSideAsA(A,c,C);
       B = calcAngle2A(A,C);
       b = calcSideA2s(B,a,c);
     }
-    else if(check([C,B,c])||check([B,C,b])){
+    else if(C&&B&&c||B&&C&&b){
       if(c) b = calcSideAsA(B,c,C);
       if(b) c = calcSideAsA(C,b,B);
       A = calcAngle2A(B,C);
       a = calcSideA2s(A,b,c);
     }
-    else if(check([B,A,b])||check([A,B,a])){
+    else if(B&&A&&b||A&&B&&a){
       if(b) a = calcSideAsA(A,b,B);
       if(a) b = calcSideAsA(B,a,A);
       C = calcAngle2A(A,B);
